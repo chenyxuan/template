79b7a0923d658d968004b411362d12fca484bfa5
lgkm39 <209985587@qq.com>
2017-07-24 15:33:39 +0800

    change indent

--- Math/FFT.cpp
+++ Math/FFT.cpp
@@ -1,24 +1,31 @@
+
 void FFT(complex *X,int n,int flag)
 {
-    for(int i = 0; i < n; i++)
-    {
-        int p = 0, t = i;
-        for(int j = 1; j < n; j <<= 1)
-          p <<= 1, p |= (t&1), t >>= 1;
-        if(i < p) std::swap(X[i], X[p]);
-    }
-    for(int m = 2; m <= n; m <<= 1)
-    {
-        complex wm = complex(cos((double)2*pi*flag/m), sin((double)2*pi*flag/m));
-        for(int i = 0 ; i < n; i += m)
-        { 
-            complex wk = complex(1, 0); 
-            for(int j = 0; j < (m>>1); wk = wk*wm, j++)
-            {
-                complex u = X[i+j], t = wk*X[i+j+(m>>1)];
-                X[i+j] = u + t, X[i+j+(m>>1)] = u - t;
-            }
-        }
-    }
-    if(flag == -1) for(int i = 0; i < n; i++) X[i] = X[i]/n;
+	for(int i = 0; i < n; i++)
+	{
+		int p = 0, t = i;
+		
+		for(int j = 1; j < n; j <<= 1)
+			p <<= 1, p |= t & 1, t >>= 1;
+		
+		if(i < p) std::swap(X[i], X[p]);
+	}
+	
+	for(int m = 2; m <= n; m <<= 1)
+	{
+		complex wm = complex(cos((double) 2 * pi * flag / m), sin((double) 2 * pi * flag / m));
+	
+		for(int i = 0; i < n; i += m)
+		{ 
+			complex wk = complex(1, 0);
+			 
+			for(int j = 0; j < (m >> 1); wk = wk * wm, j++)
+			{
+			    complex u = X[i + j], t = wk * X[i + j + (m >> 1)];
+			    X[i + j] = u + t, X[i + j + (m>>1)] = u - t;
+			}
+		}
+	}
+
+	if(flag == -1) for(int i = 0; i < n; i++) X[i] = X[i] / n;
 }
--- Math/NTT.cpp
+++ Math/NTT.cpp
@@ -1,10 +1,10 @@
 void PreWork()
 {
-    while(N <= (n<<1)) N <<= 1;
+    while(N <= (n << 1)) N <<= 1;
 
     invN = power(N, Mod - 2);
 
-    g[0] = g[N] = 1, g[1] = power(G, (Mod-1)/N); 
+    g[0] = g[N] = 1, g[1] = power(G, (Mod - 1) / N); 
     for(int i = 2; i < N; i++)
         g[i] = g[i-1] * g[1] % Mod;
 
@@ -14,21 +14,27 @@ void NTT(long long a[],int len,int flag)
     for(int i = 0; i < len; i++)
     {
         int s = i, p = 0;
-        for(int j = len>>1; j > 0; j >>= 1)
-            p |= (s&1)*j, s >>= 1;
+    
+        for(int j = len >> 1; j > 0; j >>= 1)
+            p |= (s & 1) * j, s >>= 1;
+    
         if(p > i) std::swap(a[p], a[i]);
     }
+    
     for(int L = 2 ; L <= len; L <<= 1)
         for(int i = 0; i < len ; i += L)
             for(int j = 0; j < (L>>1); j++)
             {
-                long long w = (flag == 1)?g[N/L*j]:g[N - N/L*j];
-                long long u = a[i + j], v = a[i + j +(L>>1)]*w % Mod;
-                u += v, v = u - (v<<1), a[i + j] = u % Mod;
-                a[i + j +(L>>1)] = (v + Mod) % Mod;
+                long long w = (flag == 1 ? g[N / L * j] : g[N - N / L * j]);
+                long long u = a[i + j], v = a[i + j + (L >> 1)] * w % Mod;
+    
+                u += v, v = u - (v << 1), a[i + j] = u % Mod;
+                a[i + j + (L >> 1)] = (v + Mod) % Mod;
             }
 
     if(flag == -1)
+    {
         for(int i = 0; i < len; i++)
             a[i] = a[i] * invN % Mod;
+    }
 }
